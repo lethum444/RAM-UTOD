755d429e3c47ace8ffb015aa9445182f36e14a4d
lethum444 <154496698+lethum444@users.noreply.github.com>
2024-01-31 07:44:25 +0700

    ramsong.py

--- rams/modules/ramsong.py
+++ rams/modules/ramsong.py
@@ -11,7 +11,7 @@ import os
 import pybase64
 from telethon.tl.functions.channels import JoinChannelRequest
 from telethon.tl.types import DocumentAttributeAudio
-from youtube_dl import YoutubeDL
+from yt_dlp import YoutubeDL
 from youtube_dl.utils import (
     ContentTooShortError,
     DownloadError,
